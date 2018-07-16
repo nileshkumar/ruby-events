81d853b7688bf9c30ffd166de83d2bfe08e4aa7e
Nilesh <nileshkumar.be@gmail.com>
2018-07-16 07:11:51 +0000

    add event manager

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,1 +1,14 @@
 puts "EventManager Initialized!"
+
+content = File.read "event_attendees.csv" if File.exist? "event_attendees.csv"
+lines = File.readlines "event_attendees.csv"
+
+lines.each do |line|
+  next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+  puts line.split(",")[2]
+end
+
+lines.each_with_index do |line, index|
+  next if index == 0
+  puts line.split(",")[2]
+end
